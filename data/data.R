5a589c99df02f6ac22f59aee92a9b75975c2802c
Aaron Meyer <a@ameyer.me>
2017-07-03 22:41:26 -0400

    Full model completed.

---
+++ data/data.R
@@ -0,0 +1,4 @@
+ddata <- list(ts = c(24, 96, 192, 288, 360, 456), # timepoints
+              wt_c = c(6.975, 1.579, 0.381, 0.107, 0.057, 0.025), # XXX: Last data point isn't real
+              ls_c = c(12.46, 7.44, 2.717, 1.967, 1.184, 0.540), # Fill in actual values
+              dhs_c = c(6.975, 1.579, 0.381, 0.107, 0.057, 0.025)) # Fill in actual values
