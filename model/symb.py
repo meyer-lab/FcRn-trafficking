9eac8b11a2a8a3b008a798b0b1745cdec1426c98
Aaron Meyer <a@ameyer.me>
2017-07-25 08:36:17 -0400

    Sympy model now running.

--- model/symb.py
+++ model/symb.py
@@ -1,15 +1,13 @@
-from sympy import *
+import sympy as sp
 
-t, Q, Qu, sortF, Vp = symbols('t Q Qu sortF Vp')
-Cc = Function("Cc")(t)
-Cp = Function("Cp")(t)
-Ce = Function("Ce")(t)
-releaseF = Symbol('releaseF')
-Ve = Symbol('Ve')
+t, Q, Qu, sortF, Vp, releaseF, Ve = sp.symbols('t Q Qu sortF Vp releaseF Ve')
+Cc = sp.Function("Cc")(t)
+Cp = sp.Function("Cp")(t)
+Ce = sp.Function("Ce")(t)
 
 
-Cc_ = Eq(Cc.diff(t, t), Q * (Cp - Cc))
-Cp_ = Eq(Cp.diff(t, t), (Q * Cc - Q * Cp - Qu * Cp + Qu * Ce * sortF * releaseF) / Vp)
-Ce_ = Eq(Ce.diff(t, t), Qu / Ve * (Cp + Ce * ((1 - releaseF) * sortF - 1)))
+Cc_ = sp.Eq(Cc.diff(t), Q * (Cp - Cc))
+Cp_ = sp.Eq(Cp.diff(t), (Q * Cc - Q * Cp - Qu * Cp + Qu * Ce * sortF * releaseF) / Vp)
+Ce_ = sp.Eq(Ce.diff(t), Qu / Ve * (Cp + Ce * ((1 - releaseF) * sortF - 1)))
 
-dsolve((Cc_, Cp_, Ce_), (Cc, Cp, Ce))
+sp.dsolve((Cc_, Cp_, Ce_), (Cc, Cp, Ce))
