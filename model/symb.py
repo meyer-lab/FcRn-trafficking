773ed92167ad1f181b605577a25bc6185344690c
Aaron Meyer <a@ameyer.me>
2017-07-25 15:34:10 -0400

    Unsuccessful attempt to derive an answer.

--- model/symb.py
+++ model/symb.py
@@ -1,15 +1,20 @@
 import sympy as sp
 
-t, Q, Qu, sortF, Vp, releaseF, Ve = sp.symbols('t Q Qu sortF Vp releaseF Ve',
-                                               real=True,
-                                               positive=True)
+# qq is ((1 - releaseF) * sortF - 1)
+# yy is sortF * releaseF
+
+t, Q, Qu, qq, Vp, yy, Ve = sp.symbols('t Q Qu qq Vp yy Ve', real=True, positive=True)
 Cc = sp.Function("Cc")(t)
 Cp = sp.Function("Cp")(t)
 Ce = sp.Function("Ce")(t)
 
 
 Cc_ = sp.Eq(Cc.diff(t), Q * (Cp - Cc))
-Cp_ = sp.Eq(Cp.diff(t), (Q * Cc - Q * Cp - Qu * Cp + Qu * Ce * sortF * releaseF) / Vp)
-Ce_ = sp.Eq(Ce.diff(t), Qu / Ve * (Cp + Ce * ((1 - releaseF) * sortF - 1)))
+Cp_ = sp.Eq(Cp.diff(t), (Q * Cc - Q * Cp - Qu * Cp + Qu * Ce * yy) / Vp)
+Ce_ = sp.Eq(Ce.diff(t), Qu / Ve * (Cp + Ce * qq))
 
-sp.dsolve((Cc_, Cp_, Ce_), (Cc, Cp, Ce))
+eq = (Cc_, Cp_, Ce_)
+
+print(sp.classify_ode(Ce_))
+
+sp.dsolve(eq, hint='1st_linear_Integral')
